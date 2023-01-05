da40606e8f1feb64df0940a634b0a390dff71500
ejf <ethanjfabe@gmail.com>
2023-01-05 10:30:34 +0200

    Removed function call

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main()
 {
    int x{10};
    int y{8};
-   int z{15};
+   //int z{15};
    std::cout << "y = " << y << std::endl;
    std::cout << "x = " << x << std::endl;
    std::cout << "swapping..." << std::endl;
@@ -23,9 +23,9 @@ int main()
    std::cout << "y: " << y << std::endl;
    std::cout << "x: " << x << std::endl;
 
-   std::cout << "Adding " << z << ", and " << x << std::endl;
-   auto a = add_numbers(z, x);
-   std::cout << "Answer = " << a << std::endl;
+   //std::cout << "Adding " << z << ", and " << x << std::endl;
+   //auto a = add_numbers(z, x);
+   //std::cout << "Answer = " << a << std::endl;
     
     
 }
