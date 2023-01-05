4a2f22017db54789476c9c46b5e0ac2614034704
ejf <ethanjfabe@gmail.com>
2023-01-05 10:41:47 +0200

    New project created via vs code

---
+++ README.md
@@ -0,0 +1,2 @@
+New project testing
+Practising github lol
--- main.cpp
+++ main.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
 #include <string>
 
-int add_numbers(int a, int b)
-{
-   return a + b;
-}
 
 int main()
 {
    int x{10};
    int y{8};
-   int z{15};
+   //int z{15};
    std::cout << "y = " << y << std::endl;
    std::cout << "x = " << x << std::endl;
    std::cout << "swapping..." << std::endl;
@@ -23,9 +19,9 @@ int main()
    std::cout << "y: " << y << std::endl;
    std::cout << "x: " << x << std::endl;
 
-   std::cout << "Adding " << z << ", and " << x << std::endl;
-   auto a = add_numbers(z, x);
-   std::cout << "Answer = " << a << std::endl;
+   //std::cout << "Adding " << z << ", and " << x << std::endl;
+   //auto a = add_numbers(z, x);
+   //std::cout << "Answer = " << a << std::endl;
     
     
 }
