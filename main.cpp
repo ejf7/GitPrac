e39bfcae96b3188aa7fd10317b679851a96872bc
ejf <ethanjfabe@gmail.com>
2023-01-05 10:51:03 +0200

    Readded sum function

--- main.cpp
+++ main.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 #include <string>
 
+int add_numbers(int a, int b)
+{
+   return a + b;
+}
 
 int main()
 {
    int x{10};
    int y{8};
-  
+   int z{15};
    std::cout << "y = " << y << std::endl;
    std::cout << "x = " << x << std::endl;
    std::cout << "swapping..." << std::endl;
 
-  
+   int temp{0};
 
+   temp = x;
+   x = y;
+   y = temp;
+   std::cout << "y: " << y << std::endl;
+   std::cout << "x: " << x << std::endl;
+
+   std::cout << "Adding " << z << " and " << x << std::endl;
+   auto a = add_numbers(z, x);
+   std::cout << "Answer = " << a << std::endl;
     
     
 }
